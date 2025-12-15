d45540e117d259d89a425b7c9d68dadfe6599f1a
D <K>
2025-12-15 20:21:20 +0300

    GUI

---
+++ src/Task10/GUIApp.java
@@ -0,0 +1,38 @@
+package Task10;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIApp extends JFrame {
+    JLabel welcome;
+    JButton read;
+    JButton write;
+    JButton solve;
+
+    public GUIApp() {
+        setTitle("Task10");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        welcome = new JLabel("Task10 - Объекты", SwingConstants.CENTER);
+        welcome.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        read = new JButton("Считать из файла");
+        write = new JButton("Записать в файл");
+        solve = new JButton("Решить задачу");
+        buttonPanel.add(read);
+        buttonPanel.add(write);
+        buttonPanel.add(solve);
+
+        setLayout(new BorderLayout());
+        add(welcome, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+    public static void main(String[] args) {
+        GUIApp app = new GUIApp();
+        app.setVisible(true);
+    }
+}
